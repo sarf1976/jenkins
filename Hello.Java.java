f88dd8346d6d64f86af7245ddebf27f665d94ce0
sarf1976 <35868854+sarf1976@users.noreply.github.com>
2021-11-21 09:54:40 +0530

    Update Hello.Java.java

--- Hello.Java.java
+++ Hello.Java.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window  SCM Pol implementation.
+      // Prints "Hello, World in the terminal window  SCM Pol implementation."
       System.out.println("Hello, World");
    }
 }
